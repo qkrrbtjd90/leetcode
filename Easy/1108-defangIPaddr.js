4f4144684dc5c361e7564adac7a9cd7b63b1778a
qkrrbtjd90 <qkrrbtjd90@gmail.com>
2020-03-28 00:27:16 -0400

    update

---
+++ Easy/1108-defangIPaddr.js
@@ -0,0 +1,71 @@
+//? 1108. Defanging an IP Address
+
+// Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+// A defanged IP address replaces every period "." with "[.]".
+
+// Example 1:
+
+// Input: address = "1.1.1.1"
+// Output: "1[.]1[.]1[.]1"
+// Example 2:
+
+// Input: address = "255.100.50.0"
+// Output: "255[.]100[.]50[.]0"
+
+// Constraints:
+
+// The given address is a valid IPv4 address.
+
+//todo Runtime: 48 ms, faster than 91.41% of JavaScript online submissions for Defanging an IP Address.
+//todo Memory Usage: 33.8 MB, less than 100.00% of JavaScript online submissions for Defanging an IP Address.
+//* regex
+const defangIPaddr1 = address => {
+  return address.replace(/[.]/g, '[.]');
+};
+
+//* split | join
+const defangIPaddr2 = address => {
+  return address.split('.').join('[.]');
+};
+
+//* for of
+const defangIPaddr3 = address => {
+  const newAddress = address.split('');
+  let result = '';
+
+  for (let el of newAddress) {
+    if (el === '.') result += '[.]';
+    else result += el;
+  }
+
+  return result;
+};
+
+//* forEach | ternary
+const defangIPaddr4 = address => {
+  const newAddress = address.split('');
+  let result = '';
+
+  newAddress.forEach(el => (el === '.' ? (result += '[.]') : (result += el)));
+
+  return result;
+};
+
+//! Array.from | map | join | ternary
+const defangIPaddr5 = address => {
+  return Array.from(address)
+    .map(el => (el === '.' ? '[.]' : el))
+    .join('');
+}
+
+//! split | map | join | ternary
+const defangIPaddr = address => {
+  return address
+    .split('')
+    .map(el => (el === '.' ? '[.]' : el))
+    .join('');
+}
+
+console.log(defangIPaddr('1.1.1.1')); // "1[.]1[.]1[.]1"
+console.log(defangIPaddr('255.100.50.0')); // "255[.]100[.]50[.]0"
---
+++ Easy/1365-smallerNumbersThanCurrent.js
@@ -0,0 +1,112 @@
+//? 1365. How Many Numbers Are Smaller Than the Current Number
+
+// Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
+// Return the answer in an array.
+
+// Example 1:
+
+// Input: nums = [8,1,2,2,3]
+// Output: [4,0,1,1,3]
+// Explanation:
+// For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3).
+// For nums[1]=1 does not exist any smaller number than it.
+// For nums[2]=2 there exist one smaller number than it (1).
+// For nums[3]=2 there exist one smaller number than it (1).
+// For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+
+// Example 2:
+
+// Input: nums = [6,5,4,8]
+// Output: [2,1,0,3]
+// Example 3:
+
+// Input: nums = [7,7,7,7]
+// Output: [0,0,0,0]
+
+// Constraints:
+
+// 2 <= nums.length <= 500
+// 0 <= nums[i] <= 100
+
+//* nested for loop
+const smallerNumbersThanCurrent1 = nums => {
+  let result = [];
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i += 1) {
+    let cv = nums[i];
+
+    for (let j = 0; j < nums.length; j += 1) {
+      let num = nums[j];
+
+      if (cv > num) {
+        count++;
+      }
+    }
+
+    result.push(count);
+    count = 0;
+  }
+
+  return result;
+};
+
+//! new Map | slice | sort | map | reverse | get
+const smallerNumbersThanCurrent2 = nums => {
+  const map = new Map(
+    nums
+      .slice() // create shallow copy
+      .sort((a, b) => a - b) // sort
+      .map((n, idx) => [n, idx]) // override duplicate values
+      .reverse() // reverse
+  );
+
+  console.log(map)
+
+  return nums.map(n => map.get(n));
+};
+
+//! new Map | slice | sort | forEach | if | has | set | get
+const smallerNumbersThanCurrent3 = nums => {
+  const map = new Map();
+
+  nums
+    .slice()  // shallow copy
+    .sort((a, b) => a - b) // sort
+    .forEach((n, i) => { // iterate (cv, idx)
+      if (!map.has(n)) map.set(n, i); // if map does not have n, add (num, idx)
+    });
+
+  // match num to get key from map
+  return nums.map(n => (num = map.get(n)));
+};
+
+//! new Map | slice | sort | nest for loop | has | set | get
+const smallerNumbersThanCurrent4 = nums => {
+  // creates a new instance of Map {}
+  //! slice() creates a shallow copy, then sort ascending order
+  // sort so that we can check how many are smaller
+  const map = new Map();
+  const arr = nums.slice().sort((a, b) => a - b);
+
+  // iterate through sorted array
+  // each count basically counts num of smaller num
+  // if current num NOT in map,
+  // .set(add) current number to map; {arr[i] => i} 
+  for (let i = 0; i < arr.length; i++) {
+    if (!map.has(arr[i])) map.set(arr[i], i);
+  }
+
+  // iterate through array
+  // set/replace current number to value from map
+  for (let i = 0; i < nums.length; i++) {
+    nums[i] = map.get(nums[i]);
+  }
+
+  return nums;
+};
+
+console.log(smallerNumbersThanCurrent2([8, 1, 2, 2, 3])); // [4,0,1,1,3]
+// console.log(smallerNumbersThanCurrent4([6, 5, 4, 8])); // [2,1,0,3]
+// console.log(smallerNumbersThanCurrent4([7, 7, 7, 7])); // [0,0,0,0]
