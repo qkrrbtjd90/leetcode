b4c48fdc814a7b218881508e97a4150bb43f2ef5
qkrrbtjd90 <qkrrbtjd90@gmail.com>
2020-04-04 15:26:35 -0400

    4/5

---
+++ Easy/1221. Split a String in Balanced Strings.js
@@ -0,0 +1,96 @@
+//! 1221. Split a String in Balanced Strings
+
+// Balanced strings are those who have equal quantity of 'L' and 'R' characters.
+// Given a balanced string s split it in the maximum amount of balanced strings.
+// Return the maximum amount of splitted balanced strings.
+
+// Example 1:
+// Input: s = "RLRRLLRLRL"
+// Output: 4
+// Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+
+// Example 2:
+// Input: s = "RLLLLRRRLR"
+// Output: 3
+// Explanation: s can be split into "RL", "LLLRRR", "LR", each substring contains same number of 'L' and 'R'.
+
+// Example 3:
+// Input: s = "LLLLRRRR"
+// Output: 1
+// Explanation: s can be split into "LLLLRRRR".
+
+// Example 4:
+// Input: s = "RLRRRLLRLL"
+// Output: 2
+// Explanation: s can be split into "RL", "RRRLLRLL", since each substring contains an equal number of 'L' and 'R'
+
+// Constraints:
+// 1 <= s.length <= 1000
+// s[i] = 'L' or 'R'
+
+//* for
+const balancedStringSplit1 = str => {
+  let result = 0;
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    const s = str[i]
+
+    if (count === 0) result++;
+    if (s === 'L') count ++;
+    else count--;
+  }
+
+  return result;
+}
+
+//todo map | split
+const balancedStringSplit2 = str => {
+  let result = 0;
+  let count = 0;
+
+  str.split('').map(s => {
+    if (count === 0) result++; 
+    if (s === 'L') count++;
+    else count--;
+
+  })
+
+  return result;
+}
+
+//! for | without using 'L' 'R'
+const balancedStringSplit3 = str => {
+  let result = 0;
+  let count = 1;
+
+  for (let i = 1; i < str.length; i++) {
+    str[i] === str[0] ? count++ : count--;
+
+    if (count === 0) result++
+  }
+
+  return result;
+}
+
+//! stacks
+const balancedStringSplit = str => {
+  let stack = [str[0]];
+  let result = 1;
+
+  for (let i = 1; i < str.length; i++) {
+    let s = str[i]
+
+    if (stack.length === 0) result++
+    if (s === stack[stack.length - 1] || !stack.length) stack.push(s)
+    else stack.pop()
+  }
+
+  return result;
+}
+
+console.log(balancedStringSplit('RLRRLLRLRL'))
+console.log(balancedStringSplit('RLLLLRRRLR'))
+console.log(balancedStringSplit('LLLLRRRR'))
+console.log(balancedStringSplit('RLRRRLLRLL'))
+
---
+++ Easy/1389. Create Target Array in the Given Order.js
@@ -0,0 +1,80 @@
+//* 1389. Create Target Array in the Given Order
+
+// Given two arrays of integers nums and index. Your task is to create target array under the following rules:
+
+// Initially target array is empty.
+// From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+// Repeat the previous step until there are no elements to read in nums and index.
+// Return the target array.
+
+// It is guaranteed that the insertion operations will be valid.
+
+// Example 1:
+// Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
+// Output: [0,4,1,3,2]
+// Explanation:
+// nums       index     target
+// 0            0        [0]
+// 1            1        [0,1]
+// 2            2        [0,1,2]
+// 3            2        [0,1,3,2]
+// 4            1        [0,4,1,3,2]
+
+// Example 2:
+// Input: nums = [1,2,3,4,0], index = [0,1,2,3,0]
+// Output: [0,1,2,3,4]
+// Explanation:
+// nums       index     target
+// 1            0        [1]
+// 2            1        [1,2]
+// 3            2        [1,2,3]
+// 4            3        [1,2,3,4]
+// 0            0        [0,1,2,3,4]
+
+// Example 3:
+// Input: nums = [1], index = [0]
+// Output: [1]
+
+// Constraints:
+// 1 <= nums.length, index.length <= 100
+// nums.length == index.length
+// 0 <= nums[i] <= 100
+// 0 <= index[i] <= i
+
+//* for | splice
+const createTargetArray1 = (nums, index) => {
+  let result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    const idx = index[i];
+    result.splice(idx, 0, num);
+  }
+
+  return result;
+};
+
+//todo for | shift | splice
+const createTargetArray2 = (nums, index) => {
+  let result = [];
+
+  for (let num of nums) {
+    let idx = index.shift(); // gets the first element from index array
+
+    result.splice(idx, 0, num);
+  }
+
+  return result;
+};
+
+//! reduce | concat | splice
+const createTargetArray = (nums, index) => {
+  return nums.reduce(
+    (acc, cv, i) => acc.concat(acc.splice(index[i], 0, cv)),
+    [],
+  );
+};
+
+console.log(createTargetArray([0, 1, 2, 3, 4], [0, 1, 2, 2, 1]));
+console.log(createTargetArray([1, 2, 3, 4, 0], [0, 1, 2, 3, 0]));
+console.log(createTargetArray([1], [0]));
