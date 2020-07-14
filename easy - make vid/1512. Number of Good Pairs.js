e892433a5ff252d3d88c85e0b786723d4a0b4f4e
Kyu Park <qkrrbtjd90@gmail.com>
2020-07-14 01:56:21 -0400

    update

--- easy - make vid/1512. Number of Good Pairs.js
+++ easy - make vid/1512. Number of Good Pairs.js
@@ -52,5 +52,5 @@ const numIdenticalPairs = nums => {
 
 
 console.log(numIdenticalPairs([1, 2, 3, 1, 1, 3]));
-// console.log(numIdenticalPairs([1, 1, 1, 1]));
-// console.log(numIdenticalPairs([1, 2, 3]));
+console.log(numIdenticalPairs([1, 1, 1, 1]));
+console.log(numIdenticalPairs([1, 2, 3]));
